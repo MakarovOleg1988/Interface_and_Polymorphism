53222f3b18d52136f159617d1281efa5f2fb6615
MakarovOleg1988 <makarov.oleg1988@gmail.com>
2022-11-23 11:13:32 +0530

    Добавьте файлы проекта.

---
+++ Interface_and_Polymorphism/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Code_Coach_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Draw pencil = new Draw();
+            Draw brush = new Brush();
+            Draw spray = new Spray();
+            pencil.StartDraw();
+            brush.StartDraw();
+            spray.StartDraw();
+        }
+    }
+
+    public interface IDraw
+    {
+        void StartDraw();
+    }
+
+    class Draw : IDraw
+    {
+        public virtual void StartDraw()
+        {
+            Console.WriteLine("Using pencil");
+        }
+    }
+
+    class Brush : Draw
+    {
+        public override void StartDraw()
+        {
+            Console.WriteLine("Using brush");
+        }
+    }
+
+    class Spray : Draw
+    {
+        public override void StartDraw()
+        {
+            Console.WriteLine("Using spray");
+        }
+    }
+}
